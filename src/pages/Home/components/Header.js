f709cd1cd8afa3311cb53aa1eea8806eea620daf
Lucas Nhimi <lucaslonguinhos@gmail.com>
2020-08-05 13:51:19 -0300

    Initial Code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='toolbar'>
+        <div>
+          <a href='/'>Conecta Dev - Alteração</a>
+        </div>
+        <div>
+          <button>Novo Post</button>
+          <span>img1</span>
+          <span>img2 - alteração</span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
